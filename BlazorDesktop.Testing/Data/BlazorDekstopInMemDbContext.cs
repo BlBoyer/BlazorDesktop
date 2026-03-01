33816928f7654d9e03c8d3c41e28e2c0cb224e6f
bboy <boyer.benlee@gmail.com>
2026-02-28 18:39:37 -0800

    add test project with weather subscriber test

---
+++ BlazorDesktop.Testing/Data/BlazorDekstopInMemDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BlazorDesktop.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorDesktop.Testing.Data;
+
+public static class InMemoryDb
+{
+	public static BlazorDesktopContext CreateInMemoryDb()
+	{
+		var options = new DbContextOptionsBuilder<BlazorDesktopContext>()
+			.UseSqlite("DataSource=:memory:")
+			.Options;
+
+		var context = new BlazorDesktopContext(options);
+		context.Database.OpenConnection();
+		context.Database.EnsureCreated();
+
+		return context;
+	}
+
+}
---
+++ BlazorDesktop.Testing/WeatherOrchestratorTests.cs
@@ -0,0 +1,56 @@
+﻿
+namespace BlazorDesktop.Testing;
+
+using BlazorDesktop.Application.Weather;
+using BlazorDesktop.Domain;
+using BlazorDesktop.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using TUnit;
+using static BlazorDesktop.Application.Weather.WeatherOrchestrator;
+
+public class WeatherOrchestratorTests
+{
+	[Test]
+	public async Task Subscriber_HandlesForecastsRequestAndEmitsForecastsCorrectly()
+	{
+		// Arrange
+		bool handled = false;
+		Application.Weather.WeatherForecast[]? resultForecasts = null;
+		WeatherOrchestrator.ForecastsEmitted += (e) =>
+		{
+			handled = true;
+			resultForecasts = e.Forecasts;
+		};
+
+		BlazorDesktopContext context = Data.InMemoryDb.CreateInMemoryDb();
+
+		// Create a minimal subscriber instance
+		var subscriber = new WeatherSubscriber(context);
+
+		var payload = new Domain.WeatherForecast[]
+		{
+			new (){ Date = new DateOnly(), TemperatureC = 19, Summary = "Rainy" },
+			new (){ Date = new DateOnly(), TemperatureC = 10, Summary = "Snowy" }
+		};
+		var applicationForecasts = payload.Select(x => new Application.Weather.WeatherForecast(x.Date, x.TemperatureC, x.Summary)).ToArray();
+
+		context.WeatherForecast.AddRange(payload);
+		await context.SaveChangesAsync();
+
+		var requestEvent = new ForecastsRequestedEvent();
+
+		// Act
+		//WeatherOrchestrator.RequestForecasts(); //write new test for filtered request
+		subscriber.HandleForecastsRequest(requestEvent);
+
+		// Assert
+		using (Assert.Multiple()) 
+		{
+			await Assert.That(handled, "Subscriber should handle the emitted event").IsTrue();
+			await Assert.That(resultForecasts).IsNotNull();
+			await Assert.That(resultForecasts).Count().IsEqualTo(12);	//seed data plus test data
+			await Assert.That(resultForecasts).Contains(applicationForecasts[0]);
+		}
+	}
+}
